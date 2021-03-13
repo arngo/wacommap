223272e91b58bb4bc9c45fd3bf26b2386919e52d
arngo <27396817+arngo@users.noreply.github.com>
2021-03-13 00:00:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# wacommap
+
+A command line tool written in bash using xsetwacom combined with slop to map Wacom tablets to an area selected by the cursor.
+
+## Features
+
+- Keep aspect ratio option
+- Options for mapping adjustment:
+    - Fit to selected area
+    - Fit only width to selected area
+    - Center on selected area
+- Rotation
+
+## Requirements
+
+- xf86-input-wacom
+- slop
+
+## Options
+
+**-m**
+- Using this option will enable maintaining aspect ratio. Possible values are "fit" and "width".
+- `-m fit` will shrink the mapped area to fit both width and height into the selected area
+- `-m width` will shrink the mapped area only by width
+
+**-c**
+- Using this option will center the mapped area on the selected area (otherwise it will be aligned top left). This option does nothing if used without `-m`.
+
+**-r**
+- Specify the rotation of the tablet Possible values are "none", "cw", "ccw", and "half", as with xsetwacom's "Rotate" parameter.
+
+## Example Usage
+
+This command will not maintain aspect ratio; the drawing area will be directly mapped to the selected area.
+
+```sh
+wacommap
+```
+
+This command enables maintaining aspect ratio with the "width" option, and centers the mapped area on the selected area.
+
+```sh
+wacommap -m width -c
+```
---
+++ wacommap.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+device=$(xsetwacom list | grep STYLUS | awk '{print $9}')
+
+if [[ -z "$device" ]]; then
+    echo "Tablet not found."
+    exit 1
+fi
+
+center=0
+rot=none
+mode=''
+
+while getopts ":m:r:c" flag
+do
+    case "${flag}" in
+        m)
+            mode=${OPTARG}
+            if [[ "$mode" != "fit" && "$mode" != "width" ]]; then
+                echo "Invalid mode: $mode"
+                exit 1
+            fi
+            ;;
+        r)
+            rot=${OPTARG}
+            if [[ "$rot" != "cw" && "$rot" != "ccw" && "$rot" != "half" && "$rot" != "none" ]]
+            then
+                echo "Invalid rotation: $rot"
+                exit 1
+            else
+                xsetwacom set $device Rotate $rot
+            fi
+            ;;
+        c)
+            center=1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+
+slop=$(slop -f "%x %y %w %h %g") || exit 1
+read -r X Y W H G < <(echo $slop)
+area=$(xsetwacom get $device area)
+read -r TX TY TW TH < <(echo $area)
+
+# swap tablet width and height if rotated
+if [[ "$rot" == "cw" || "$rot" == ccw ]]; then
+    temp=$TH
+    TH=$TW
+    TW=$temp
+fi
+
+mapwidth=$W
+mapheight=$H
+
+if [[ -n $mode ]]; then
+    if [[ "$mode" == "fit" ]]; then
+        mapwidth=$((H*TW/TH))
+    fi
+
+    if [[ "$mapwidth" -gt "$W" || "$mode" == "width" ]]; then
+        mapwidth=$W
+        mapheight=$((W*TH/TW))
+    fi
+fi
+
+if [[ $center -eq 1 ]]; then
+    diffW=$((W-mapwidth))
+    diffH=$((H-mapheight))
+
+    X=$((X+diffW/2))
+    Y=$((Y+diffH/2))
+fi
+
+echo ${W}x${H}
+echo $X+$Y
+echo ${mapwidth}x${mapheight}
+echo $X+$Y
+
+xsetwacom set $device MapToOutput ${mapwidth}x${mapheight}+$X+$Y
